4b0a404215ceaf03d6464589651e0af004bee426
GOYAM JAIN <goyamjain02@gmail.com>
2025-05-11 16:48:32 +0530

    added active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,25 @@ const scrollUp = () =>{
     this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop-58,
+        sectionId = current.getAttribute('id'),
+        sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        if(scrollY > sectionTop && scrollY <=sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+        }else{
+            sectionClass.classList.remove('active-link')
+        }
+
+
+    })
+}
+window.addEventListener('scroll', scrollActive)
