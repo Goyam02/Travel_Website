cd117a4e18d37948cc68d826e90a165d98d7b829
GOYAM JAIN <goyamjain02@gmail.com>
2025-05-11 16:10:32 +0530

    fixed minor issue in show menu and added scrollup

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,10 @@ const blurHeader = () =>{
     this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
+
+
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
